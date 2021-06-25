c47633702c033926f300d93207ff4c94d548f139
S M Kaiser Ahmed <53963906+mekaiser@users.noreply.github.com>
2021-06-25 21:32:45 +0600

    ongoing

--- src/components/Home/Header/HeaderMain/HeaderMain.js
+++ src/components/Home/Header/HeaderMain/HeaderMain.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import repairMan from '../../../../images/repair-man.png';
-import './HeaderMain.css';
+import React from "react";
+import repairMan from "../../../../images/repair-man.png";
+import "./HeaderMain.css";
 
 const HeaderMain = () => {
-    return (
-        <div className="row header-main-focus-container">
-            <div className="col-md-8">
-                <h1 style={{fontWeight: '700'}} className="text-white  header-main-focus-text">Excellent Services For Repairing Your <span style={{fontSize: '1.4em', color: '#fd374e'}}>Broken Gadget</span></h1>
-                <button type="button" className="btn btn-custom">Get Started</button>
-            </div>
-            <div className="col-md-4 text-center">
-                <img className="repairman-img" src={repairMan} alt="" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="row header-main-focus-container">
+      <div className="col-md-8">
+        <h1
+          style={{ fontWeight: "700" }}
+          className="text-white  header-main-focus-text"
+        >
+          Excellent Services For Repairing Your{" "}
+          <span className="header-main-focus-text-span">Broken Gadget</span>
+        </h1>
+        <button type="button" className="btn btn-custom">
+          Get Started
+        </button>
+      </div>
+      <div className="col-md-4 text-center">
+        <img className="repairman-img" src={repairMan} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default HeaderMain;
